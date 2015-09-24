57f95ccd940ee080fb6ad79f3a91fb547ba13e3d
carlo379 <admin@dactylmobile.com>
2015-09-24 22:57:59 +0000

    Added Other Team Members APIs Methods

--- posts/posts.js
+++ posts/posts.js
@@ -3,9 +3,9 @@
 */
 var OAuth = require('oauth');
 
-module.exports = function(app){
+module.exports = function(app) {
 
-    // '/tweetStatus' - Post to Twitter - By Carlos
+    // '/tweetStatus' - Post to Twitter - By Carlos Martinez
     app.post('/tweetStatus', function (req, res) {
         var oauth = new OAuth.OAuth(
             'https://api.twitter.com/oauth/request_token',
@@ -20,15 +20,18 @@ module.exports = function(app){
             'https://api.twitter.com/1.1/statuses/update.json',
             process.env.TWITTER_USER_TOKEN, //test user token
             process.env.TWITTER_USER_SECRET, //test user secret
-            {"status":req.body.status},
+            {"status": req.body.status},
             function (e, data) {
-                if (e) console.error(e);
-                console.log(require('util').inspect(data));
-                res.send(data);
+                if (e) {
+                    res.send("error");
+                } else {
+                    console.log(require('util').inspect(data));
+                    res.send(data);
+                }
             });
     });
 
-    // '/friendList' - Get from Twitter - By Carlos
+    // '/friendList' - Get from Twitter - By Carlos Martinez
     app.post('/friendList', function (req, res) {
         var oauth = new OAuth.OAuth(
             'https://api.twitter.com/oauth/request_token',
@@ -44,16 +47,19 @@ module.exports = function(app){
             process.env.TWITTER_USER_TOKEN, //test user token
             process.env.TWITTER_USER_SECRET, //test user secret
             function (e, data) {
-                if (e) console.error(e);
-                console.log(require('util').inspect(data));
-                res.send(data);
+                if (e) {
+                    res.send("error");
+                } else {
+                    console.log(require('util').inspect(data));
+                    res.send(data);
+                }
             });
     });
 
-    // '/searchTweets  Get (Search) on Twitter - Snehal
+    // '/searchTweets  Get (Search) on Twitter - Snehal Golhar
     app.post('/searchTweet', function (req, res) {
         var encodedText = encodeURIComponent(req.body.searchWord);
-        console.log("Search Text: "+encodedText);
+        console.log("Search Text: " + encodedText);
         var oauth = new OAuth.OAuth(
             'https://api.twitter.com/oauth/request_token',
             'https://api.twitter.com/oauth/access_token',
@@ -64,18 +70,20 @@ module.exports = function(app){
             'HMAC-SHA1'
         );
         oauth.get(
-            'https://api.twitter.com/1.1/search/tweets.json?q='+encodedText,
+            'https://api.twitter.com/1.1/search/tweets.json?q=' + encodedText,
             process.env.TWITTER_USER_TOKEN, //test user token
             process.env.TWITTER_USER_SECRET, //test user secret
-            function (e, data, result) {
-                console.log("In return function...Search");
-                //if (e) console.error(e);
-                console.log(require('util').inspect(data));
-                res.send(data);
+            function (e, data) {
+                if (e) {
+                    res.send("error");
+                } else {
+                    console.log(require('util').inspect(data));
+                    res.send(data);
+                }
             });
     });
 
-    // '/getfollowers  Get (Search) on Twitter - Snehal
+    // '/getfollowers  Get (Search) on Twitter - Snehal Golhar
     app.post('/getfollowers', function (req, res) {
         var oauth = new OAuth.OAuth(
             'https://api.twitter.com/oauth/request_token',
@@ -91,9 +99,167 @@ module.exports = function(app){
             process.env.TWITTER_USER_TOKEN, //test user token
             process.env.TWITTER_USER_SECRET, //test user secret
             function (e, resultfollowersResponse) {
-                if (e) console.error(e);
-                console.log(require('util').inspect(resultfollowersResponse));
-                res.send(resultfollowersResponse);
+                if (e) {
+                    res.send("error");
+                } else {
+                    console.log(require('util').inspect(resultfollowersResponse));
+                    res.send(resultfollowersResponse);
+                }
             });
     });
-}
+
+    // '/friendsIds' - Get from Twitter - By Shalini Negi
+    app.post('/friendsIds', function (req, res) {
+        var oauth = new OAuth.OAuth(
+            'https://api.twitter.com/oauth/request_token',
+            'https://api.twitter.com/oauth/access_token',
+            process.env.TWITTER_CONSUMER_KEY,
+            process.env.TWITTER_CONSUMER_SECRET,
+            '1.0A',
+            null,
+            'HMAC-SHA1'
+        );
+        oauth.get(
+            'https://api.twitter.com/1.1/friends/ids.json',
+            process.env.TWITTER_USER_TOKEN, //test user token
+            process.env.TWITTER_USER_SECRET, //test user secret
+            function (e, data) {
+                if (e) {
+                    res.send("error");
+                } else {
+                    console.log(require('util').inspect(data));
+                    res.send(data);
+                }
+            });
+    });
+
+    // '/followersIds' - Post to Twitter - By Shalini Negi
+    app.post('/followersIds', function (req, res) {
+        var oauth = new OAuth.OAuth(
+            'https://api.twitter.com/oauth/request_token',
+            'https://api.twitter.com/oauth/access_token',
+            process.env.TWITTER_CONSUMER_KEY,
+            process.env.TWITTER_CONSUMER_SECRET,
+            '1.0A',
+            null,
+            'HMAC-SHA1'
+        );
+        oauth.get(
+            'https://api.twitter.com/1.1/followers/ids.json',
+            process.env.TWITTER_USER_TOKEN, //test user token
+            process.env.TWITTER_USER_SECRET, //test user secret
+            function (e, data) {
+                if (e) {
+                    res.send("error");
+                } else {
+                    console.log(require('util').inspect(data));
+                    res.send(data);
+                }
+            });
+    });
+
+    // '/recentStatus' - Post to Twitter - By Monisha Dash
+    app.post('/recentStatus', function (req, res) {
+        var user_name1 = encodeURIComponent(req.body.timeline_newuser);
+        console.log("New User" + user_name1);
+
+        var oauth = new OAuth.OAuth(
+            'https://api.twitter.com/oauth/request_token',
+            'https://api.twitter.com/oauth/access_token',
+            process.env.TWITTER_CONSUMER_KEY,
+            process.env.TWITTER_CONSUMER_SECRET,
+            '1.0A',
+            null,
+            'HMAC-SHA1'
+        );
+        oauth.get(
+            'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=' + user_name1,
+            process.env.TWITTER_USER_TOKEN, //test user token
+            process.env.TWITTER_USER_SECRET, //test user secret
+            function (e, data) {
+                if (e) {
+                    res.send("error");
+                } else {
+                    console.log(require('util').inspect(data));
+                    res.send(data);
+                }
+            });
+    });
+
+    // '/createNewFollower' - Post to Twitter - By Monisha Dash
+    app.post('/createNewFollower', function (req, res) {
+        var screen_name = encodeURIComponent(req.body.nameToRemove);
+
+        var oauth = new OAuth.OAuth(
+            'https://api.twitter.com/oauth/request_token',
+            'https://api.twitter.com/oauth/access_token',
+            process.env.TWITTER_CONSUMER_KEY,
+            process.env.TWITTER_CONSUMER_SECRET,
+            '1.0A',
+            null,
+            'HMAC-SHA1'
+        );
+        oauth.post(
+            'https://api.twitter.com/1.1/friendships/create.json',
+            process.env.TWITTER_USER_TOKEN, //test user token
+            process.env.TWITTER_USER_SECRET, //test user secret
+            {"screen_name":req.body.nameToRemove},
+            function (e, data) {
+                if (e) {
+                    res.send("error");
+                } else {
+                    console.log(require('util').inspect(data));
+                    res.send(data);
+                }
+            });
+    });
+
+    // '/removeFriend' - Post to Twitter - By Khine Oo (Clara)
+    app.post('/removeFriend', function(req,res){
+        //get screen_name user entered
+        var encoded_friendname=encodeURIComponent(req.body.friendName);
+
+        var oauth = new OAuth.OAuth(
+            'https://api.twitter.com/oauth/request_token',
+            'https://api.twitter.com/oauth/access_token',
+            process.env.TWITTER_CONSUMER_KEY,
+            process.env.TWITTER_CONSUMER_SECRET,
+            '1.0A',
+            null,
+            'HMAC-SHA1'
+        );
+        oauth.post('https://api.twitter.com/1.1/friendships/destroy.json?',
+            process.env.TWITTER_USER_TOKEN, //test user token
+            process.env.TWITTER_USER_SECRET, //test user secret
+            {"screen_name":req.body.friendName},
+            function(e, data){
+                if (e) {
+                    res.send("error");
+                } else {
+                    console.log(require('util').inspect(data));
+                    res.send(data);
+                }
+            });
+    });
+
+    // '/suggestionList' - Post to Twitter - By Khine Oo (Clara)
+    app.post('/suggestionList', function(req,res){
+        var oauth = new OAuth.OAuth(
+            'https://api.twitter.com/oauth/request_token',
+            'https://api.twitter.com/oauth/access_token',
+            process.env.TWITTER_CONSUMER_KEY,
+            process.env.TWITTER_CONSUMER_SECRET,
+            '1.0A',
+            null,
+            'HMAC-SHA1'
+        );
+        oauth.get('https://api.twitter.com/1.1/users/suggestions.json',
+            process.env.TWITTER_USER_TOKEN, //test user token
+            process.env.TWITTER_USER_SECRET, //test user secret
+            function (e, data) {
+                if (e) console.error(e);
+                console.log(require('util').inspect(data));
+                res.send(data);
+            });
+    });
+};
--- public/js/twitter_hw.js
+++ public/js/twitter_hw.js
@@ -22,6 +22,9 @@ $(document).ready(function(){
         // Prevent the default action of submitting the form to the server
         e.preventDefault();
 
+        // Erase message to get it ready for next tweet.
+        $("#tweetMessage").html("");
+
         // Capture the value of the text field - "message"
         var message = $("#status").val();
 
@@ -39,8 +42,11 @@ $(document).ready(function(){
                     $('#tweetMessage').html("<p>Tweet was NOT possible! Try Again</p>");
                 }
                 else {
+                    // parse JSON data to variable
+                    var tweetedMessage = JSON.parse(data);
+
                     // Insert Result Message in Division
-                    $('#tweetMessage').html("<p>The following message was Tweeted: <br><strong>" + message + "</strong></p>");
+                    $('#tweetMessage').html("<p>The following message was Tweeted: <br><strong>" + tweetedMessage.text + "</strong></p>");
                     // Erase Text Field to get it ready for next tweet.
                     $("#status").val("");
                 }
@@ -104,20 +110,19 @@ $(document).ready(function(){
         // Execute Posting only if the validation of the Form was successful
         if($("#searchtweetForm").validationEngine('validate')) {
             // http POST communication with the NODEJS Server - Sending Captured Text
-            $.post('/searchTweet', {searchWord: searchword}, function (data) {
+            $.post('/searchTweet', {searchWord: searchword}, function (searchTweetResponse) {
 
                 // Make the result area visible
                 $('#searchBox').removeClass("hidden");
 
                 // Verify if the returned data contains the message "error"
-                if (data == "error") {
+                if (searchTweetResponse == "error") {
                     // Insert Error Message in Division
                     $('#searchResultsMessage').html("<p>No Result for your search </p>");
-                    console.log("Error");
                 }
                 else {
                     // parse JSON data to variable
-                    searchResults = JSON.parse(data);
+                    searchResults = JSON.parse(searchTweetResponse);
                     console.log(searchResults);
                     // Clear message area (Division) from previous messages
                     $('#searchResultsMessage').html("");
@@ -134,6 +139,12 @@ $(document).ready(function(){
                         // Increment variable by 1
                         a++;
                     });
+                    if(a==1){
+                        // If we still at a = 1, then ge did not get results -Append message that no result were found
+                        $('#searchResultsMessage').append(
+                            "<p>Your search did not return any result! Try Again!</p>"
+                        );
+                    }
                 }
             });
         }
@@ -178,6 +189,267 @@ $(document).ready(function(){
         });
     });
 
+    /* -- Twitter API: "GET Friends/Ids"  -  By Shalini Negi
+     ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+    $("#getFriendIdBtn").click(function() {
+
+        // Variable to get friend ids from twitter
+        var friendId;
+
+        // http POST communication with the NODEJS Server
+        $.post('/friendsIds', function(data) {
+
+            // Make the result area visible
+            $('#getFriendIdDiv').removeClass("hidden");
+
+            // Verify if the returned data contains the message "error"
+            if(data == "error") {
+                // Insert Error Message in Division
+                $('#getFriendIdMessage').html("<p>Could Not Get Friend Ids! Try Again</p>");
+            }
+            else {
+                // parse JSON data to variable
+                friendId = JSON.parse(data);
+
+                // Clear message area (Division) from previous messages
+                $('#getFriendIdMessage').html("");
+
+                // Variable to count friend ids and add it to the List
+                var countFriendIds = 1;
+
+                // Go through each object (friend ids) in the array
+                $.each(friendId.ids, function() {
+                    // Get each friend ids and add it to the message area as item on list
+                    $('#getFriendIdMessage').append(
+                        "<li id='" + countFriendIds + "'>" + this + "</li>"
+                    );
+                    // Increment variable by 1
+                    countFriendIds++;
+                });
+            }
+        });
+    });
+
+    /* -- Twitter API: "GET Followers/Ids"  -  By Shalini Negi
+     ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+    $("#getFollowerIdBtn").click(function() {
+
+        // Variable to get followers ids from twitter
+        var followersIds;
+
+        // http POST communication with the NODEJS Server
+        $.post('/followersIds', function(data) {
+
+            // Make the result area visible
+            $('#getFollowerIdDiv').removeClass("hidden");
+
+            // Verify if the returned data contains the message "error"
+            if(data == "error") {
+                // Insert Error Message in Division
+                $('#getFollowerIdMessage').html("<p>Could Not Get Followers Ids! Try Again</p>");
+            }
+            else {
+                // parse JSON data to variable
+                followersList = JSON.parse(data);
+
+                // Clear message area (Division) from previous messages
+                $('#getFollowerIdMessage').html("");
+
+                // Variable to count followers ids and add it to List
+                var followerVariable = 1;
+
+                // Go through each object (followers ids) in the array to get the "followers ids"
+                $.each(followersList.ids, function() {
+                    // Get each followers ids and add it to the message area as item on list
+                    $('#getFollowerIdMessage').append(
+                        "<li id='" + followerVariable + "'>" + this + "</li>"
+                    );
+                    // Increment variable by 1
+                    followerVariable++;
+                });
+            }
+        });
+    });
+
+    /* -- Twitter API: "GET Statuses/User_timeline"  -  By Monisha Dash
+     ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+    $("#getRecentStatusBtn").click(function (e) {
+        // Prevent the default action of submitting the form to the server
+        e.preventDefault();
+
+        // Variable to get timeline results from twitter
+        var recentStatus;
+
+        // Capture the value of the text field - "screen name"
+        var timeline_user = $("#screenName").val();
+
+
+        // http POST communication with the NODEJS Server - Sending Captured Text
+        $.post('/recentStatus', {timeline_newuser: timeline_user}, function (data) {
+
+            // Make the result area visible
+            $('#statusBox').removeClass("hidden");
+
+            // Verify if the returned data contains the message "error"
+            if (data == "error") {
+                // Insert Error Message in Division
+                $('#recentStatusList').html("<p>Could Not Get Recent Status! Try Again</p>");
+            }
+            else {
+                // parse JSON data to variable
+                recentStatus = JSON.parse(data);
+                // Clear message area (Division) from previous messages
+                $('#recentStatusList').html("");
+
+                // Variable to count users and add it as the List id
+                var a = 1;
+
+                // Go through each object (statuses) in the array to get the "screen name" and "timeline"
+                $.each(recentStatus, function () {
+                    // Append the User Screen Name only on first iteration
+                    if (a == 1) {
+                        $('#recentStatusList').append(
+                            "<h4><strong>Timeline for User: </strong>" + this.user.name + "</h4>"
+                        );
+                    }
+                    // Append the timeline of messages tweeted; then add (append) to list as HTML
+                    $('#recentStatusList').append(
+                        "<li id='" + a + "'>" + this.text + "</li>"
+                    );
+                    // Increment variable by 1
+                    a++;
+                });
+            }
+        });
+    });
+
+    /* -- Twitter API: "POST Friendships/Create"  -  By Monisha Dash
+     ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+    $("#getNewFollowersBtn").click(function (e) {
+        // Prevent the default action of submitting the form to the server
+        e.preventDefault();
+
+        // Variable to get Screen Name of user to follow in twitter
+        var screenName;
+
+        // Capture the value of the text field - "screen name"
+        var newFriend = $("#screen_name").val();
+
+        // Execute Posting only if the validation of the Form was successful
+        if($("#createFollowerForm").validationEngine('validate')) {
+
+            // http POST communication with the NODEJS Server - Sending Captured Text
+            $.post('/createNewFollower', {nameToRemove:newFriend}, function(data) {
+
+                // Make the result area visible
+                $('#createFriendshipBox').removeClass("hidden");
+
+                // Verify if the returned data contains the message "error"
+                if (data == "error") {
+                    // Insert Error Message in Division
+                    $('#newFriendshipList').html("<p>No such user name</p>");
+                }
+                else {
+
+                    // parse JSON data to variable
+                    screenName = JSON.parse(data);
+                    console.log("On FrontEnd Resp: " + data);
+                    console.log("On FrontEnd Resp: " + screenName);
+
+                    // Clear message area (Division) from previous messages
+                    $('#newFriendshipList').html("");
+
+                    // Append the name of the new Friend
+                    $('#newFriendshipList').append("<p><strong>Now You Are Following: </strong>" + screenName.name + "</p>");
+
+                    // Erase Text Field to get it ready for next tweet.
+                    $("#screen_name").val("");
+
+                }
+            });
+        }
+    });
+
+    /* -- Twitter API: "POST friendships/Destory"  -  By Khine Oo(Clara)
+     ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+    $("#removeFriendBtn").click(function(e){
+        // Prevent the default action of submitting the form to the server
+        e.preventDefault();
+
+        //Variable to remove a friend from friends on Twitter
+        var removeFriend;
+
+        // Capture the value of the text field - "friend name" to remove
+        var friendname =$("#removingFriend").val();
+
+        // Execute Posting only if the validation of the Form was successful
+        if($("#removefriendForm").validationEngine('validate')) {
+            // http POST communication with the NODEJS Server - Sending Captured Text
+            $.post('/removeFriend', {friendName: friendname}, function (data) {
+
+                // Make the result area visible
+                $('#removeFriendBox').removeClass("hidden");
+
+                if (data == "error") {
+                    // Insert Error Message in Division
+                    $('#removeFriendMessage').html("<p>Failed to remove this friend.</p>");
+                }
+                else {
+                    // parse JSON data to variable
+                    removeFriend = JSON.parse(data);
+                    console.log("Result Console Front: " + data);
+                    $('#removeFriendMessage').html("<p>The Screen Name <strong>" + removeFriend.name + " </strong>was removed from your Twitter friend.</p>");
+
+                    // Erase Text Field to get it ready for next tweet.
+                    $("#removingFriend").val("");
+                }
+            });
+        }
+    });
+
+    /* -- Twitter API: "GET Users/Suggestions"  -  By Khine Oo(Clara)
+     ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+    $("#getSuggestionListBtn").click(function (e){
+        e.preventDefault();
+
+        //Variable to get Suggestion list from twitter
+        var suggestionList;
+
+        // http POST communication with the NODEJS Server
+        $.post('/suggestionList',function(data){
+
+            // Make the result area visible
+            $('#divsuggestionList').removeClass("hidden");
+
+            // Verify if the returned data contains the message "error"
+            if(data=="error"){
+                // Insert Error Message in Division
+                $('#suggestionListMessage').html("<p>Sorry! Could not provide the Suggestion List in the mean time! </p>");
+            }
+            else{
+                // parse JSON data to variable
+                suggestionList =JSON.parse(data);
+
+                // Clear message area (Division) from previous messages
+                $('#suggestionList').html("");
+
+                // Variable to count Suggestions ids and add it to List
+                var a =1;
+
+                // Go through each object (followers ids) in the array to get the "followers ids"
+                $.each(suggestionList, function(){
+                    // Get each user ids and add it to the message area as item on list
+                    $('#suggestionList').append(
+                        "<li id='" +a+"'>"+this.name+"</li>"
+                    );
+
+                    // Increment variable by 1
+                    a++;
+                });
+            }
+        });
+    });
+
 
     /* Validation Attachment Area
     -------------------------------------------- */
@@ -187,6 +459,11 @@ $(document).ready(function(){
     //Form Validation Tweet form
     $("#tweetForm").validationEngine('attach', {promptPosition : "topLeft"});
 
+    //Form Validationfor Create Follower
+    $("#createFollowerForm").validationEngine('attach', {promptPosition : "topLeft"});
+
+    //Form Validation Remove Followers
+    $("#removefriendForm").validationEngine('attach', {promptPosition : "topLeft"});
 });
  
 /* Individual Functions
@@ -197,10 +474,10 @@ function changeBackgroundHeight() {
 
       //Adjust Height and bottom accordingly
 	  if (width < 750) {
-		$(".carousel .item").height(90);
+		$(".carousel .item").height(25);
 		$(".carousel-caption").css("bottom","0");
 	  } else {
-		$(".carousel .item").height(90);
+		$(".carousel .item").height(50);
 		$(".carousel-caption").css("bottom","-20");
 	  }
 }
